5ed93a28c18bf1eba962457e58ca5219a3a74d23
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-10-15 17:05:57 +0200

    update UserInterface.hpp

--- memory/src/UserInterface.hpp
+++ memory/src/UserInterface.hpp
@@ -12,13 +12,13 @@ namespace mg {
         int board_height;
         
     public:
-        Board                input_board(void);
-        std::pair<int, int>  input_card_position(const Board& board) const;
-        void                 wait_for_input(void) const;
-        void                 print_board(const Board& board) const;
-        void                 print_equal_position_warning(void) const;
-        void                 print_whose_turn(int index) const;
-        void                 print_summary(const Player* players) const ;
+        Board               input_board(void);
+        std::pair<int, int> input_card_position(const Board& board) const;
+        void                wait_for_input(void) const;
+        void                print_board(const Board& board) const;
+        void                print_equal_position_warning(void) const;
+        void                print_whose_turn(int index) const;
+        void                print_summary(const Player* players) const ;
     };
 }
 

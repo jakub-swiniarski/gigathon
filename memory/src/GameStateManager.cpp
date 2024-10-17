fe179fc7598a9267f9cbdae627b87dc2ff8652b9
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-10-17 17:51:35 +0200

    this is a mess

--- memory/src/GameStateManager.cpp
+++ memory/src/GameStateManager.cpp
@@ -2,7 +2,12 @@
 
 #include "GameStateManager.hpp"
 
-void mg::GameStateManager::save_board(std::ofstream& file, Board& board) {
+mg::GameStateManager::GameStateManager(std::string filepath)
+    : filepath(filepath) {}
+
+void mg::GameStateManager::save(Board& board, Player* players, int whose_turn) const {
+    std::ofstream file(filepath);
+
     file << board.matrix_width << ' ' << board.matrix_height << '\n';
     for (int y = 0; y < board.matrix_height; y++) {
         file << board.card_matrix[y];
@@ -13,19 +18,8 @@ void mg::GameStateManager::save_board(std::ofstream& file, Board& board) {
             file << board.mask_matrix[y][x];
         file << '\n';
     }
-}
 
-mg::GameStateManager::GameStateManager(std::string filepath)
-    : filepath(filepath) {}
-
-void mg::GameStateManager::load(Board& board, Player* players, int& whose_turn) {
-
-}
-
-void mg::GameStateManager::save(Board& board, Player* players, int whose_turn) {
-    std::ofstream file(filepath);
-
-    save_board(file, board);
     file << players[0].get_score() << ' ' << players[1].get_score() << '\n';
+
     file << whose_turn;
 } 
--- memory/src/GameStateManager.hpp
+++ memory/src/GameStateManager.hpp
@@ -1,21 +1,19 @@
 #ifndef GAMESTATEMANAGER_HPP
 #define GAMESTATEMANAGER_HPP
 
+#include <fstream>
+
 #include "Board.hpp"
 #include "Player.hpp"
-#include <fstream>
 
 namespace mg {
     class GameStateManager {
         const std::string filepath;
 
-        void save_board(std::ofstream& file, Board& board);
-
     public:
         GameStateManager(std::string filepath);
         
-        void load(Board& board, Player* players, int& whose_turn);
-        void save(Board& board, Player* players, int whose_turn);
+        void save(Board& board, Player* players, int whose_turn) const;
     };
 }
 

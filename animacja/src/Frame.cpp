075f6429aab28ece974b0e134570e576509ae164
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-10-18 21:36:18 +0200

    frame class

---
+++ animacja/src/Frame.cpp
@@ -0,0 +1,41 @@
+#include <stdexcept>
+
+#include "Frame.hpp"
+
+ani::Frame::Frame(Vector size, std::ifstream& file) 
+    : size(size),
+      char_matrix(size.second, std::string(size.first, 0)),
+      color_matrix(size.second, std::vector<int>(size.first, 0)) {
+    std::string line;
+
+    for (int y = 0; y < size.second; y++) {
+        getline(file, line);
+
+        if (line.size() != static_cast<size_t>(size.second))
+            throw std::runtime_error("Nieprawidlowy opis klatki.");
+
+        char_matrix[y] = line;
+    }
+
+    for (int y = 0; y < size.second; y++) {
+        getline(file, line);
+
+        if (line.size() != static_cast<size_t>(size.second))
+            throw std::runtime_error("Nieprawidlowy opis klatki.");
+
+        for (int x = 0; x < size.first; x++)
+            color_matrix[y][x] = line[x] - '0';
+    }
+}
+
+ani::Vector ani::Frame::get_size(void) const {
+    return size;
+}
+
+char ani::Frame::get_char(Vector position) const {
+    return char_matrix[position.second][position.first];
+}
+
+int ani::Frame::get_color(Vector position) const {
+    return color_matrix[position.second][position.first];
+}
---
+++ animacja/src/Frame.hpp
@@ -0,0 +1,28 @@
+#ifndef FRAME_HPP
+#define FRAME_HPP
+
+#include <fstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace ani {
+    using Vector      = std::pair<int, int>;
+    using CharMatrix  = std::vector<std::string>;
+    using ColorMatrix = std::vector<std::vector<int>>; 
+
+    class Frame {
+        Vector      size;
+        CharMatrix  char_matrix;
+        ColorMatrix color_matrix;
+
+    public:
+        Frame(Vector size, std::ifstream& file);
+
+        Vector get_size(void) const;
+        char   get_char(Vector position) const;
+        int    get_color(Vector position) const;
+    };
+}
+
+#endif // FRAME_HPP

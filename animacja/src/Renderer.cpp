cad34a1d5b9fcaf5dbe249bce2dab33202e6d3c7
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-10-19 11:39:33 +0200

    reset color

--- animacja/src/Renderer.cpp
+++ animacja/src/Renderer.cpp
@@ -4,15 +4,16 @@
 
 ani::Renderer::Renderer(Vector frame_size)
     : frame_size(frame_size),
-      colors(8) {
-    colors[0] = "\033[30m";
-    colors[1] = "\033[31m";
-    colors[2] = "\033[32m";
-    colors[3] = "\033[33m";
-    colors[4] = "\033[34m";
-    colors[5] = "\033[35m";
-    colors[6] = "\033[36m";
-    colors[7] = "\033[37m";
+      colors(9) {
+    colors[0] = "\033[0m";
+    colors[1] = "\033[30m";
+    colors[2] = "\033[31m";
+    colors[3] = "\033[32m";
+    colors[4] = "\033[33m";
+    colors[5] = "\033[34m";
+    colors[6] = "\033[35m";
+    colors[7] = "\033[36m";
+    colors[8] = "\033[37m";
     // TODO: improve this, save suffix and prefx separately
 }
 

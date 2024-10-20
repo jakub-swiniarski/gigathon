b6ce23a7669769a520681d61ad10d8a426343de6
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-10-20 10:29:26 +0200

    improvements

--- animacja/src/Renderer.cpp
+++ animacja/src/Renderer.cpp
@@ -1,22 +1,18 @@
 #include <chrono>
 #include <iostream>
+#include <string>
 #include <thread>
 
 #include "Renderer.hpp"
 
 ani::Renderer::Renderer(Vector frame_size)
     : frame_size(frame_size),
-      colors(9) {
-    colors[0] = "\033[0m";
-    colors[1] = "\033[30m";
-    colors[2] = "\033[31m";
-    colors[3] = "\033[32m";
-    colors[4] = "\033[33m";
-    colors[5] = "\033[34m";
-    colors[6] = "\033[35m";
-    colors[7] = "\033[36m";
-    colors[8] = "\033[37m";
-    // TODO: improve this, save suffix and prefx separately
+      color_prefix("\033["),
+      color_suffix("m"),
+      colors(n_colors) {
+    colors[0] = "0";
+    for (int i = 1; i < n_colors; i++)
+        colors[i] = std::to_string(29 + i);
 }
 
 void ani::Renderer::render(const Frame& frame) const {
@@ -24,13 +20,17 @@ void ani::Renderer::render(const Frame& frame) const {
     std::cout << '\a';
     for (int y = 0; y < frame_size.second; y++) {
         for (int x = 0; x < frame_size.first; x++) {
-            std::cout << colors[frame.get_color({x, y})];
+            print_color(frame.get_color({x, y}));
             std::cout << frame.get_char({ x, y });
         }
         std::cout << '\n';
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    std::cout << colors.front();
+    print_color(0);
+}
+
+void ani::Renderer::print_color(int index) const {
+    std::cout << color_prefix << colors[index] << color_suffix;
 }
 
 void ani::Renderer::cmd_clear(void) const {
--- animacja/src/Renderer.hpp
+++ animacja/src/Renderer.hpp
@@ -9,12 +9,17 @@
 namespace ani {
     class Renderer {
         Vector                   frame_size;
+        std::string              color_prefix;
+        std::string              color_suffix;
         std::vector<std::string> colors;
+
+        static constexpr int n_colors = 9;
 
     public:
         Renderer(Vector frame_size);
 
         void render(const Frame& frame) const;
+        void print_color(int index) const;
         void cmd_clear(void) const;
     };
 }
